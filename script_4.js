ed44c63c143e633f5af11712fa097a5ebe675481
aeseb <sebastoledano@outlook.com>
2022-06-02 14:55:09 -0300

    Problema 4

---
+++ script_4.js
@@ -0,0 +1,8 @@
+
+
+var galletasPorCaja = 5;
+var cajasCompradas = 4;
+
+var totalGalletas = galletasPorCaja * cajasCompradas;
+
+console.log(`Si compramos 4 cajas tenemos ${totalGalletas} galletas.`);
