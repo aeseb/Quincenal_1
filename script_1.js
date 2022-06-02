aac0bbd6703b18fdfd025daa9209ce6459bd212c
aeseb <sebastoledano@outlook.com>
2022-06-02 14:53:41 -0300

    Problema 1

---
+++ script_1.js
@@ -0,0 +1,9 @@
+// Programa de operaciones simples: suma, asignacion.
+
+var manzanasMaria = 5;
+var manzanasPedro = 8;
+var manzanasSusana = 9;
+
+var manzanasTotales = manzanasMaria + manzanasPedro + manzanasSusana;
+
+console.log("Manzanas en la canasta: ", manzanasTotales);
