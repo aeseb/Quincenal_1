4fa2eac54305a1d26cdd06e215f926db45e2c3a6
aeseb <sebastoledano@outlook.com>
2022-06-02 14:54:08 -0300

    Problema 2

---
+++ script_2.js
@@ -0,0 +1,8 @@
+
+
+var amigosDeJuan = 12;
+var madalenasTotales = 28;
+
+var madalenasRestantes = madalenasTotales - amigosDeJuan - 1;
+
+console.log(`Restan ${madalenasRestantes} madalenas, las manzanas me las comi yo.`);
